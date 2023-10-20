d6dddb1d9bc0b2d252af381c0dd4ba70fbcfae17
Mona Lisa <email@example.com>
2023-10-20 11:16:02 +0000

    add base rule test

---
+++ mock/makeTest.sh
@@ -0,0 +1,20 @@
+node fix.js > ../../../log/1.csv 
+wait
+node fix.js > ../../../log/2.csv 
+wait
+node fix.js > ../../../log/3.csv 
+wait
+node fix.js > ../../../log/4.csv 
+wait
+node fix.js > ../../../log/5.csv 
+wait
+node fix.js > ../../../log/6.csv 
+wait
+node fix.js > ../../../log/7.csv 
+wait
+node fix.js > ../../../log/8.csv 
+wait
+node fix.js > ../../../log/9.csv 
+wait
+node fix.js > ../../../log/10.csv 
+wait
